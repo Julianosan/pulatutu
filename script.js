d8acba0208a1f434d6709bd5d5e4d18480830b67
Juliano <59764017+Julianosan@users.noreply.github.com>
2022-05-28 16:13:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const tutu = document.querySelector('.tutu')
+const dino = document.querySelector('.dino')
+
+const jump = () => {
+    tutu.classList.add('jump')
+
+    setTimeout(() => {
+    
+    tutu.classList.remove('jump')
+    
+    }, 500)
+
+    window.getSelection().removeAllRanges()
+
+}
+
+const loop = setInterval(() => {
+
+    const dinoPosition = dino.offsetLeft
+    const tutuPosition = +window.getComputedStyle(tutu).bottom.replace('px', '')
+
+    if (dinoPosition <= 290 && dinoPosition > 200 && tutuPosition < 80){
+        
+        dino.style.animation = 'none'
+        dino.style.left = `${dinoPosition}px`
+
+        tutu.style.animation = 'none'
+        tutu.style.bottom = `${tutuPosition}px`
+
+        tutu.src = 'tutu-gameover.png'
+        tutu.style.width = '80px'
+        tutu.style.marginLeft = '220px'
+
+        clearInterval(loop)
+
+    } 
+
+
+}, 10)
+
+
+document.addEventListener("click", jump)
+document.addEventListener("keydown", jump)
